1c3817b4c381f9e36bee5c97cba9e4a2f888bbbf
Данил Петров <cras.petrov@yandex.ru>
2025-03-28 11:42:15 +0200

    Fix GlobalNotFound page

--- src/app/providers/root-provider/index.tsx
+++ src/app/providers/root-provider/index.tsx
@@ -1,6 +1,7 @@
 import { NextIntlClientProvider } from 'next-intl';
 import { FC, ReactNode } from 'react';
 import { ThemeProvider } from 'next-themes';
+import { getMessages } from 'next-intl/server';
 
 import { textFont, titleFont } from '@/app/fonts';
 import { Header } from '@/widgets/header';
@@ -11,15 +12,18 @@ interface RootProviderProps {
   children: ReactNode;
 }
 
-export const RootProvider: FC<RootProviderProps> = async ({ locale, children }) => (
-  <html className={`${titleFont.variable} ${textFont.variable}`} lang={locale} suppressHydrationWarning>
-    <body>
-      <ThemeProvider enableColorScheme={false} defaultTheme='system' enableSystem disableTransitionOnChange>
-        <NextIntlClientProvider>
-          <Header />
-          {children}
-        </NextIntlClientProvider>
-      </ThemeProvider>
-    </body>
-  </html>
-);
+export const RootProvider: FC<RootProviderProps> = async ({ locale, children }) => {
+  const messages = await getMessages();
+  return (
+    <html className={`${titleFont.variable} ${textFont.variable}`} lang={locale} suppressHydrationWarning>
+      <body>
+        <ThemeProvider enableColorScheme={false} defaultTheme='system' enableSystem disableTransitionOnChange>
+          <NextIntlClientProvider messages={messages}>
+            <Header />
+            {children}
+          </NextIntlClientProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
